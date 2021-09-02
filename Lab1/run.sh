f79e0825c94ebdef88e2fb809cbcb39a7ec53a31
Rodrigo Rios <rodrigo.rras@gmail.com>
2021-09-02 00:37:51 -0300

    improving it

--- Lab1/run.sh
+++ Lab1/run.sh
@@ -6,53 +6,56 @@ echo "Downloading rke package 1.2.12 from Rancher"
 echo "###########################################"
 echo " "
 
-if [ -f rke_linux-amd64 ]
+if [ -f /usr/local/bin/rke ]
 then
 echo "File is found, Skipping Download.."
 echo "" 
 else
-   echo "File is not found"
+   echo "File is not found! Downloading it with Wget from Rancher availabe releases"
    wget --no-check-certificate https://github.com/rancher/rke/releases/download/v1.2.12/rke_linux-amd64
+   echo -n "\o/"
+   sleep 1.0
+   echo -n "_o_"
+   sleep 1.0
+   echo -n "\o/"
+   sleep 1.0
+   echo -n "_o_"
+   echo -e
+
+
+   echo "#################################################"
+   echo "Updating it name to rke .. giving it exec permission"
+   echo "#################################################"
+   echo " "
+
+   echo "mv rke_linux_amd64 rke"
+   mv rke_linux-amd64 rke
+   echo "chmod +x rke"
+   chmod +x rke
+   echo ""
+
+
+   echo "#################################################"
+   echo "Moving rke to /usr/local/bin .. needs sudo passwd!"
+   echo "#################################################"
+   echo " "
+
+   echo "sudo mv rke /usr/local/bin"
+   sudo mv rke /usr/local/bin
+
+
+   echo -n "\o/"
+   sleep 1.0
+   echo -n "_o_"
+   sleep 1.0
+   echo -n "\o/"
+   sleep 2.0
+   echo -n "_o_"
+   echo ""
+   echo -e "Rancher is installed.."
 fi
 
 
-echo -n "\o/"
-sleep 1.0
-echo -n "_o_"
-sleep 1.0
-echo -n "\o/"
-sleep 1.0
-echo -n "_o_"
-echo -e
 
-
-echo "#################################################"
-echo "Updating name to rke .. giving it exec permission"
-echo "#################################################"
-echo " "
-
-echo "mv rke_linux_amd64 rke"
-mv rke_linux-amd64 rke
-echo "chmod +x rke"
-chmod +x rke
-echo ""
-
-
-echo "#################################################"
-echo "Moving rke to /usr/local/bin .. needs sudo passwd!"
-echo "#################################################"
-echo " "
-
-echo "sudo mv rke /usr/local/bin"
-sudo mv rke /usr/local/bin
-
-
-echo -n "\o/"
-sleep 1.0
-echo -n "_o_"
-sleep 1.0
-echo -n "\o/"
-sleep 2.0
-echo -n "_o_"
-echo -e "end.."
-
+echo "rke --version"
+rke --version
